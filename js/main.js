73d0dcd5324cef094cf6c2c411560dcc998a28af
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-04-09 16:43:43 -0700

    solutions sections ui

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ console.log("I work");
 new WOW().init();
 
 
-$('section').horizon();
+$('section').horizon(
+	{
+    scrollTimeout: null,
+    scrollEndDelay: 250,
+    scrollDuration: 400,
+    i: 0,
+    limit: 0,
+    docWidth: 0,
+    sections: null,
+    swipe: true,
+    fnCallback: function (i) {}
+});
 
 // $.jInvertScroll(['.scroll']);
