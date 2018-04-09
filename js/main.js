25b4726ca8689d95077842a8c0e4a368612a6af1
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-04-08 20:48:30 -0700

    ugh

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@ console.log("I work");
 
 new WOW().init();
 
+// $(document).ready(function() {
+//     $('#cta, #solutions').mousewheel(function(e, delta) {
+//         // multiplying by 40 is the sensitivity, 
+//         // increase to scroll faster.
+//         this.scrollLeft -= (delta * 40);
+//         e.preventDefault();
+//     });
+// });
+
+// (function($) {
+// 	$.jInvertScroll(['#cta', '#solutions']);
+// }(jQuery));
+
+
+// $(document).ready(function(){
+// 		$('html, body').animate({scrollTop: 0}, 120);
+// 	});
+
 // var rellax = new Rellax('.rellax', {
 //         // center: true
 //         callback: function(position) {
