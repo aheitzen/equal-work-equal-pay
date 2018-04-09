42efebbd124aa959772bba29a9575140389b8ca0
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-04-09 15:25:57 -0700

    horizontal scroll

--- js/main.js
+++ js/main.js
@@ -2,28 +2,7 @@ console.log("I work");
 
 new WOW().init();
 
-// $(document).ready(function() {
-//     $('#cta, #solutions').mousewheel(function(e, delta) {
-//         // multiplying by 40 is the sensitivity, 
-//         // increase to scroll faster.
-//         this.scrollLeft -= (delta * 40);
-//         e.preventDefault();
-//     });
-// });
 
-// (function($) {
-// 	$.jInvertScroll(['#cta', '#solutions']);
-// }(jQuery));
+$('section').horizon();
 
-
-// $(document).ready(function(){
-// 		$('html, body').animate({scrollTop: 0}, 120);
-// 	});
-
-// var rellax = new Rellax('.rellax', {
-//         // center: true
-//         callback: function(position) {
-//             // callback every position change
-//             console.log('position');
-//         }
-// });
+// $.jInvertScroll(['.scroll']);
