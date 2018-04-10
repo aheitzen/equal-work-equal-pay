8616ee8458cfba23b1f568675d7150fa54228de9
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-04-10 12:28:06 -0700

    hillarys first code section, contemporary leaders and definition section

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # equal-work-equal-pay
 
 <!-- Ignore this part -alexa -->
+
+
+<div>
+  <video></video>
+  <div id="image"></div>
+</div>
+
+position relative on parent div
+video position relative
+background image div “equal pay” position absolute
--- js/main.js
+++ js/main.js
@@ -3,17 +3,18 @@ console.log("I work");
 new WOW().init();
 
 
-$('section').horizon(
-	{
-    scrollTimeout: null,
-    scrollEndDelay: 250,
-    scrollDuration: 400,
-    i: 0,
-    limit: 0,
-    docWidth: 0,
-    sections: null,
-    swipe: true,
-    fnCallback: function (i) {}
-});
+// $('section').horizon(
+// 	{
+//     scrollTimeout: null,
+//     scrollEndDelay: 250,
+//     scrollDuration: 400,
+//     i: 0,
+//     limit: 0,
+//     docWidth: 0,
+//     sections: null,
+//     swipe: true,
+//     fnCallback: function (i) {}
+// });
+
 
 // $.jInvertScroll(['.scroll']);
