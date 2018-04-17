16e55f8149f448782628cf4dea49453598dc6ac8
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-04-17 14:39:13 -0700

    solutions

--- js/main.js
+++ js/main.js
@@ -16,24 +16,63 @@ $(document).ready(function() {
     });
   });
 
+    $( "#family" ).hover(
+  function() {
+    $( "#family-sub-header-hover" ).css( "display", "none" );
+    $(".showTextFamily").css({
+      "display": "block",
+      "animation": "fadein 1s"
+    });
+    $("#family-header-hover").css({
+      "margin-bottom": "1vh",
+      "transition": "ease-out .4s"
+    });
+    }, function() {
+    $( "#family-sub-header-hover" ).css( "display", "block" );
+    $( ".showTextFamily" ).css( "display", "none" );
+    $("#family-header-hover").css({
+      "margin-bottom": "0vh",
+      "transition": "ease-out .4s"
+    });
+  }
+);
   $( "#political" ).hover(
   function() {
     $( "#political-sub-head-hover" ).css( "display", "none" );
-    // $( ".showText" ).css( "display", "block" );
     $(".showText").css({
       "display": "block",
       "animation": "fadein 1s"
     });
     $("#political-header-hover").css({
-      "margin-bottom": "3vh",
-      "transition": "ease-out .5s"
+      "margin-bottom": "2vh",
+      "transition": "ease-out .4s"
     });
     }, function() {
     $( "#political-sub-head-hover" ).css( "display", "block" );
     $( ".showText" ).css( "display", "none" );
     $("#political-header-hover").css({
       "margin-bottom": "0vh",
-      "transition": "ease-out .5s"
+      "transition": "ease-out .4s"
+    });
+  }
+);
+    $( "#financial" ).hover(
+  function() {
+    $( "#financial-sub-head-hover" ).css( "display", "none" );
+    $(".showTextFinancial").css({
+      "display": "block",
+      "animation": "fadein 1s"
+    });
+    $("#financial-header-hover").css({
+      "margin-bottom": "3vh",
+      "transition": "ease-out .4s"
+    });
+    }, function() {
+    $( "#financial-sub-head-hover" ).css( "display", "block" );
+    $( ".showTextFinancial" ).css( "display", "none" );
+    $("#financial-header-hover").css({
+      "margin-bottom": "0vh",
+      "transition": "ease-out .4s"
     });
   }
 );
