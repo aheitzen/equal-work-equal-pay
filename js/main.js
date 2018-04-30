a964331a37038310ad3c2d8fe57aee10280b88a3
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-04-29 18:56:41 -0700

    recommit

--- js/main.js
+++ js/main.js
@@ -493,57 +493,51 @@ var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Recreation", "Desingers", "Biologist"],
         datasets: [{
-            label: 'Median annual pay',
-            data: [24 + "k", 56 + "k", 55 + "k"],
+            label: 'Before female dominated',
+            // data: [53 + "k", 84 + "k", 67 + "k"],
+            data: [53, 84, 67],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(21, 46, 70, 0.7)',
+                'rgba(21, 46, 70, 0.7)',
+                'rgba(21, 46, 70, 0.7)'
+                
+                // 'rgba(255, 99, 132, 0.2)',
+                // 'rgba(255, 99, 132, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgb(21, 46, 70)',
+                'rgb(21, 46, 70)',
+                'rgb(21, 46, 70)'
+                // 'rgba(255,99,132,1)',
+                // 'rgba(255,99,132,1)'
             ],
-            borderWidth: 1
+            borderWidth: 2
         },{
             label: 'After female dominated',
-            data: [12, 19, 3],
+            data: [24, 56, 55],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(44, 96, 165, 0.7)',
+                'rgba(44, 96, 165, 0.7)',
+                'rgba(44, 96, 165, 0.7)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgb(44, 96, 165)',
+                'rgb(44, 96, 165)',
+                'rgb(44, 96, 165)'
             ],
-            borderWidth: 1
+            borderWidth: 2
         }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+        // scales: {
+        //     yAxes: [{
+        //         ticks: {
+        //             beginAtZero:true
+        //         }
+        //     }]
+        // }
     }
 });
 
